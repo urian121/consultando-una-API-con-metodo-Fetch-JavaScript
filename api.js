19dd11ebce8d99c2498249fd35d039256db09005
Urian Viera <urian1213viera@example.com>
2023-03-25 21:01:32 -0500

    iniciando git

--- api.js
+++ api.js
@@ -1,63 +1,27 @@
+async function obtenerPerroAleatorio() {
+  const preloader = document.querySelector("#preloader");
 
-/** Forma basica */
-fetch('examples/example.json')
-  .then(function (response) {
-    //Aquí es donde pones lo que quieres hacer con la respuesta.**
+  try {
+    preloader.style.display = "block"; // mostrar el preloader
 
-  })
-  .catch(function (error) {
-    console.log('Oh No! There was a problem: \n', error);
-  });
-
-
-/** Validando una respuesta */
-fetch('examples/example.json')
-  .then(function (response) {
+    const URL_API = "https://dog.ceo/api/breeds/image/random";
+    const response = await fetch(URL_API);
     if (!response.ok) {
-      throw Error(response.statusText);
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    // Aquí es donde pones lo que quieres hacer con la respuesta.
-  })
-  .catch(function (error) {
-    console.log('Looks like there was a problem: \n', error);
-  });
-
-/**forma 3 */
-fetch('examples/example.json')
-  .then(function (response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    // Read the response as json.
-    return response.json();
-  })
-  .then(function (responseAsJson) {
-    // Do stuff with the JSON
-    console.log(responseAsJson);
-  })
-  .catch(function (error) {
-    console.log('Looks like there was a problem: \n', error);
-  });
-
-/** Usando el método post con Fetch */
-fetch('https://example.com/users.json', {
-  method: 'POST',
-  mode: 'cors',
-  redirect: 'follow',
-  headers: new Headers({
-    'Content-Type': 'text/plain'
-  })
-}).then(function () {
-  /* manejar la respuesta */
-});
-
-/** Otro método con fetch */
-fetch('https://example.com/users', {
-  method: 'PUT', // or 'POST'
-  body: JSON.stringify(data), // data can be `string` or {object}!
-  headers: {
-  'Content-Type': 'application/json'
+    const data = await response.json();
+    return data.message; // devuelve la URL de la imagen del perro
+  } catch (error) {
+    console.error(`Error en la consulta a la API: ${error}`);
+  } finally {
+    preloader.style.display = "none"; // ocultar el preloader
+  }
 }
-}).then(res => res.json())
-  .then(response => console.log('Success:', JSON.stringify(response)))
-  .catch(error => console.error('Error:', error));
+
+async function mostrarPerro() {
+  const imagen = await obtenerPerroAleatorio();
+
+  document.querySelector("#imagen-perro").innerHTML = `
+          <img src="${imagen}" alt="Perro aleatorio">
+        `;
+}
