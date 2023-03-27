81696e2e4b6f743aa9e1607aabf76dd84118e7d5
Urian Viera <urian1213viera@example.com>
2023-03-26 20:56:09 -0500

    ajustes y agregando comentarios

--- api.js
+++ api.js
@@ -1,15 +1,23 @@
+/*
+ * Creando una función asincrona llamada obtenerPerroAleatorio
+ * la misma retorna la respuesta la solicitud en dicha API
+ */
 async function obtenerPerroAleatorio() {
   const preloader = document.querySelector("#preloader");
 
   try {
     preloader.style.display = "block"; // mostrar el preloader
 
+    //URL de la API
     const URL_API = "https://dog.ceo/api/breeds/image/random";
     const response = await fetch(URL_API);
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      console.log(`HTTP error! status: ${response.status}`);
     }
+    //Obtener los datos en formato JSON
     const data = await response.json();
+
+    console.log(data.message);
     return data.message; // devuelve la URL de la imagen del perro
   } catch (error) {
     console.error(`Error en la consulta a la API: ${error}`);
@@ -18,10 +26,17 @@ async function obtenerPerroAleatorio() {
   }
 }
 
+/**
+ * La función mostrarPerro() llama a la función obtenerPerroAleatorio()
+ * y la misma retorna la solicitud de una consulta a la API de mascotas, si la respuesta es
+ * correcta retorna una imagen (URL de mascota)
+ * La función mostrarPerro() pinta la respuesta obtenida desde la función obtenerPerroAleatorio()
+ * en la vista atraves del identificador con id='imagen-perro'
+ */
 async function mostrarPerro() {
-  const imagen = await obtenerPerroAleatorio();
+  const resp = await obtenerPerroAleatorio();
 
   document.querySelector("#imagen-perro").innerHTML = `
-          <img src="${imagen}" alt="Perro aleatorio">
+          <img src="${resp}" alt="Perro aleatorio">
         `;
 }
